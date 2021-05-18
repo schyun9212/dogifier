86c3a7deb3a6d4d7b7d8b7cdced4e1f685e61df1
schyun9212 <schyun9212@gmail.com>
2021-05-18 14:44:04 +0000

    add baseline

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="dog picture classifier",
+    version="0.1",
+    author="schyun9212",
+    author_email="schyun9212@gmail.com",
+    description="Implementation to classify dog drawings",
+    packages=find_packages(),
+)
