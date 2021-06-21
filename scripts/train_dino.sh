9324e9a6f31563bee313d3634d1ffa34f534d2d0
schyun9212 <schyun9212@gmail.com>
2021-06-21 16:52:30 +0000

    add dino training script

---
+++ scripts/train_dino.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ~/dogifier
+
+model_names=("dino_vits16" "dino_vits8" "dino_vitb16" "dino_vitb8")
+batch_sizes=(512 512 256 256)
+
+for i in "${!model_names[@]}";
+do
+    echo ${model_names[$i]}
+    echo ${batch_sizes[$i]}
+    python main.py \
+        model=dino \
+        expr=${model_names[$i]} \
+        datamodule.batch_size=${batch_sizes[$i]}
+    
+    python main.py \
+        model=dino \
+        model.freeze_backbone=False \
+        expr=${model_names[$i]}_finetune \
+        datamodule.batch_size=${batch_sizes[$i]}
+done
