ef79bef720b545e08f043018d397aef2ecb20fd6
schyun9212 <schyun9212@gmail.com>
2021-06-21 16:01:05 +0000

    add assert and implement rollback imagenet function

--- tools/utils.py
+++ tools/utils.py
@@ -3,8 +3,9 @@ import os
 import random
 import shutil
 
-def split_imagefolder(root, image_folder="train", split_ratio=0.2):
+def split_imagefolder(root, image_folder="train", split_ratio=0.05):
     image_folder = os.path.join(root, image_folder)
+    assert os.path.exists(image_folder)
     val_dir = os.path.join(root, "val")
     os.mkdir(val_dir)
     
@@ -20,3 +21,20 @@ def split_imagefolder(root, image_folder="train", split_ratio=0.2):
             src_path = os.path.join(src_dir, val_image)
             dst_path = os.path.join(dst_dir, val_image)
             shutil.move(src_path, dst_path)
+
+def rollback_imagefolder(root, src_folder="val", dst_folder="train"):
+    image_folder = os.path.join(root, src_folder)
+    assert os.path.exists(image_folder)
+
+    for label in os.listdir(image_folder):
+        src_dir = os.path.join(image_folder, label)
+        dst_dir = os.path.join(root, dst_folder, label)
+        assert os.path.exists(dst_dir)
+
+        images = os.listdir(src_dir)
+        for image in images:
+            src_path = os.path.join(src_dir, image)
+            dst_path = os.path.join(dst_dir, image)
+            shutil.move(src_path, dst_path)
+    
+    os.rmdir(image_folder)
