a55dc160d15103971148a7ab1a3721cdd6ecd3e7
schyun9212 <schyun9212@gmail.com>
2021-07-27 21:59:24 +0900

    separate train and test

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ from omegaconf import DictConfig
 from dogifier.datamodules.build import build_datamodule
 from dogifier.model import Dogifier
 from dogifier.callback import build_checkpoint_callback, build_early_stop_callback
-from dogifier.utils import get_num_params, save_ckpt_from_result, get_expr_name
 
 
 @hydra.main(config_path="conf", config_name="config")
@@ -25,16 +24,7 @@ def main(cfg: DictConfig) -> None:
         callbacks=callbacks
     )
 
-    if cfg.mode == "fit":
-        if get_num_params(model, requires_grad=True) > 0:
-            trainer.fit(model, datamodule=dm)
-        else:
-            val_result = trainer.validate(model, datamodule=dm)[0]
-            save_ckpt_from_result(trainer, val_result, model_dir)
-    elif cfg.model == "val":
-        trainer.validate(model, datamodule=dm)
-    elif cfg.mode == "test":
-        trainer.test(model, dataomodule=dm)
+    trainer.fit(model, datamodule=dm)
 
 
 if __name__ == "__main__":
